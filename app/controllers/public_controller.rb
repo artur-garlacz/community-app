9ca4d2798717da288d6fcd724954b51b3f2a8135
Administrator <admin@MBPAdmiistrator.fritz.box>
2020-08-14 23:44:03 +0200

    Added profile view & listing posts

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,10 @@
 class PublicController < ApplicationController
     def index
         @communities = Community.all.limit(5)
+        @posts = Post.order(id: :desc).limit(20)
+    end
+
+    def profile 
+        @profile = Account.find_by_username params[:username]
     end
 end
