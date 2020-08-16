adad591950d1df2b12f554796a17c2ae3856d336
Administrator <admin@MBPAdmiistrator.fritz.box>
2020-08-16 23:01:37 +0200

    New migrations, added subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200806064916) do
+ActiveRecord::Schema.define(version: 20200816200723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,13 @@ ActiveRecord::Schema.define(version: 20200806064916) do
     t.datetime "updated_at",                null: false
     t.index ["account_id"], name: "index_posts_on_account_id", using: :btree
     t.index ["community_id"], name: "index_posts_on_community_id", using: :btree
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "account_id"
+    t.integer "community_id"
+    t.index ["account_id"], name: "index_subscriptions_on_account_id", using: :btree
+    t.index ["community_id"], name: "index_subscriptions_on_community_id", using: :btree
   end
 
   create_table "ticket_types", force: :cascade do |t|
